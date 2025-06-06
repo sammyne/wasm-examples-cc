a6c92e79771d49df8bbfeecc922314ec5ffecef9
sammyne <xiangmin-li@qq.com>
2025-06-06 00:45:29 +0000

    feat(docker): add sccache to speed up rust build

--- play.sh
+++ play.sh
@@ -4,6 +4,7 @@ cd `dirname ${BASH_SOURCE[0]}`
 
 repo_tag=sammyne/wasm-studio-cc:9543549
 cargo_home=$PWD/_cargo
+sccache_home=$PWD/_sccache
 workdir=/github.com/sammyne/wasm-examples-cc
 
 name=github-wasm-studio-cc
@@ -13,9 +14,11 @@ docker rm -f $name
 docker run -td --rm                               \
   --name $name                                    \
   -e CARGO_HOME=/root/.cargo                      \
+  -e SCCACHE_CONF=/root/.sccache/config.toml      \
   -v $PWD:$workdir                                \
   -v $cargo_home/registry:/root/.cargo/registry   \
   -v $cargo_home/git:/root/.cargo/git             \
+  -v $sccache_home:/root/.sccache                 \
   -w $workdir                                     \
   $repo_tag bash
 
