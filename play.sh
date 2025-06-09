c55a60371df97c472ae296597ddaec27817b6ee1
sammyne <xiangmin-li@qq.com>
2025-06-09 03:41:50 +0000

    feat(play.sh): add gitconfig and sccache config

--- play.sh
+++ play.sh
@@ -2,28 +2,34 @@
 
 cd `dirname ${BASH_SOURCE[0]}`
 
-repo_tag=sammyne/wasm-studio-cc:9543549
-cargo_home=$PWD/_cargo
-sccache_home=$PWD/_sccache
+repo_tag=sammyne/wasm-studio-cc:02b7b1a
+
+cache_dir=$PWD/_cache
 workdir=/github.com/sammyne/wasm-examples-cc
+
+volume_opts="-v $PWD:$workdir"
+if [[ -d $PWD/../.. ]]; then
+  volume_opts="$volume_opts -v $PWD/../..:/github.com"
+fi
 
 name=github-wasm-studio-cc
 
 docker rm -f $name
 
-docker run -td --rm                               \
-  --name $name                                    \
-  -e CARGO_HOME=/root/.cargo                      \
-  -e SCCACHE_CONF=/root/.sccache/config.toml      \
-  -v $PWD:$workdir                                \
-  -v $cargo_home/registry:/root/.cargo/registry   \
-  -v $cargo_home/git:/root/.cargo/git             \
-  -v $sccache_home:/root/.sccache                 \
-  -w $workdir                                     \
+docker run -td --rm                                   \
+  --name $name                                        \
+  -e CARGO_HOME=$workdir/_cache/cargo                 \
+  -e SCCACHE_CONF=$workdir/_cache/sccache/config.toml \
+  $volume_opts                                        \
+  -w $workdir                                         \
   $repo_tag bash
 
-if [ -d _ssh ]; then
-  docker cp _ssh $name:/root/.ssh
+if [ -d $cache_dir/ssh ]; then
+  docker cp $cache_dir/ssh $name:/root/.ssh
+fi
+
+if [ -f $cache_dir/gitconfig ]; then
+  docker cp $cache_dir/gitconfig $name:/root/.gitconfig
 fi
 
 docker exec -it $name bash
