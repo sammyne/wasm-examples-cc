689fd56c354f5ca5792eee59ea468daba2626396
sammyne <xiangmin-li@qq.com>
2026-01-16 01:49:39 +0000

    feat(component-model): demo global-var

---
+++ component-model/global-var/guest/include/global-var/hack.h
@@ -0,0 +1,19 @@
+#ifndef __MEMORY_INSPECTOR_HACK_H
+#define __MEMORY_INSPECTOR_HACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// man 3 malloc 输出中的 SYNOPSIS 小节列出的函数应该都需要包装。
+
+void *__wrap_calloc(size_t nmemb, size_t size);
+
+void __wrap_free(void *ptr);
+
+void *__wrap_malloc(size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ component-model/global-var/guest/src/lib.cc
@@ -0,0 +1,54 @@
+#include "helloworld.h"
+#include "global-var/hack.h"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+
+// 冗余的 __real_* 函数声明是必须的
+extern "C" {
+  void *__real_calloc(size_t nmemb, size_t size);
+  void __real_free(void *ptr);
+  void *__real_malloc(size_t);
+}
+
+void *__wrap_calloc(size_t nmemb, size_t size) {
+  return __real_calloc(nmemb, size);
+}
+
+void __wrap_free(void *ptr) {
+  printf("free(%p)\n", ptr);
+  __real_free(ptr);
+}
+
+void *__wrap_malloc(size_t size) {
+  // 不能直接调用 malloc，否则递归导致栈溢出。
+  auto p = __real_malloc(size);
+
+  printf("malloc(%zu)=%p\n", size, p);
+
+  return p;
+}
+
+std::string dummy(const char *tag) {
+  auto out = std::string("dummy");
+  printf("[dummy-%s] addr(out)=%p, addr(out.c_str())=%p)\n", tag, &out, out.c_str());
+  return out;
+}
+
+static std::string HELLO_WORLD = dummy("static");
+
+std::string HELLO_WORLD2 = dummy("non-static");
+
+// ref: https://github.com/grpc/grpc-go/blob/master/examples/helloworld/greeter_server/main.go#L43
+void exports_sammyne_helloworld_greeter_say_hello(exports_sammyne_helloworld_greeter_hello_request_t *req, exports_sammyne_helloworld_greeter_hello_reply_t *ret) {
+  std::string message((const char*)(req->name.ptr), req->name.len);
+
+  message = std::string("Hello ") + message;
+
+  printf("HELLO_WORLD.c_str=%p\n", HELLO_WORLD.c_str());
+
+  helloworld_string_dup(&ret->message, message.c_str());
+}
