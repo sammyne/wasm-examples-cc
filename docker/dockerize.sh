7757489acaa829b9fca14b79855b92f68cffae2f
sammyne <xiangmin-li@qq.com>
2026-01-17 14:07:03 +0000

    feat(docker): use wasmtime@22.0.1, wasi-sdk@22.0, wasm-tools@1.212.0, wit-bindgen@0.40.0

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-build_arg_opts="$build_arg_opts --build-arg WASI_SDK_MAJOR_VERSION=25"
+build_arg_opts="$build_arg_opts --build-arg WASI_SDK_MAJOR_VERSION=22"
 build_arg_opts="$build_arg_opts --build-arg WASI_SDK_MINOR_VERSION=0"
-build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=30.0.0"
-build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.227.0"
+build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=20.0.1"
+build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.212.0"
 build_arg_opts="$build_arg_opts --build-arg WIT_BINDGEN_VERSION=0.40.0"
 
-repo_tag=sammyne/wasm-studio-cc:`git rev-parse --short HEAD`
+repo_tag=sammyne/wasm-studio-cc:wt20-ws22-`git rev-parse --short HEAD`
 
 docker build $build_arg_opts -t $repo_tag .
 
-#docker push $repo_tag
+docker push $repo_tag
